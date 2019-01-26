a908f362609e867c944b81c3e026717ea98b7ab8
Aytac <aytac_07@msn.om>
2019-01-26 10:49:50 +0100

    sample scene

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    private float verticalVelocity = 0;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+
+    // Use this for initialization
+    void Start () {
+        controller = GetComponent<CharacterController>();
+
+        // let the gameObject fall down
+        gameObject.transform.position = new Vector3(0, 5, 0);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (controller.isGrounded)
+        {
+            if (Input.GetButton("Jump"))
+            {
+                verticalVelocity = jumpSpeed;
+            }
+            else
+            {
+                verticalVelocity = 0;
+            }
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal") * speed, verticalVelocity, Input.GetAxis("Vertical") * speed);
+        moveDirection = transform.TransformDirection(moveDirection);
+        controller.Move(moveDirection * Time.deltaTime);
+
+    }
+}
